9b75ac08f882f7630b11f4427d6f0e93dc079e77
Lehlogonolo Poole <hlonipoole692@gmail.com>
2024-06-27 13:19:21 +0200

    Console Logging

--- BP/scripts/scirptevent.ts
+++ BP/scripts/scirptevent.ts
@@ -8,25 +8,36 @@ system.afterEvents.scriptEventReceive.subscribe((arg) => {
 
     if (msg === "true" || msg === "false") {
         debug = JSON.parse(msg); // hopefully a boolean value
+        console.log("finna debug");
     }
+
+    console.log("passed this shit.");
 
     try {
         const entity = arg.initiator;
-
+        console.log("getting entity");
         const players = world.getAllPlayers();
         const player: Player | undefined = players.find(
             (player) => player.id === entity?.id
         );
 
+        console.log("found player");
+
         const block: Block | undefined = player?.getBlockFromViewDirection({
             maxDistance: 40,
         })?.block;
+
+        console.log("found block");
 
         if (!block) return;
 
         const { x, y, z } = block.location;
 
+        console.log("block xyz");
+
         player?.runCommand(`setblock ${x} ${y} ${z} ${block.typeId}`);
+
+        console.log("putting block back");
 
         player
             ?.getComponent("inventory")
@@ -34,7 +45,9 @@ system.afterEvents.scriptEventReceive.subscribe((arg) => {
                 player.selectedSlotIndex,
                 new ItemStack(block.typeId, 1)
             );
+        console.log("giving to player.");
         player?.runCommand(`title @s actionbar §l${block.typeId}`);
+        console.log("completee.");
     } catch (error) {
         if (debug) {
             console.error(error);
